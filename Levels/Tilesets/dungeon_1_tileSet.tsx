c5c3a7ea2b4985813b3f7ee41f967210c066ecf6
tjuyeda <tjuyeda@gmail.com>
2014-05-06 16:31:31 -0700

    Created a dungeon map
    Dungeon map is dungeon_1.txt
    I figured this is the closest one to Link's starting position and will
    give him his first sword.

---
+++ Levels/Tilesets/dungeon_1_tileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="28894" tilewidth="16" tileheight="16">
+ <image source="../../../../../Desktop/28894.gif" width="1024" height="960"/>
+</tileset>
---
+++ Levels/Tilesets/tiletype16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiletype16" tilewidth="16" tileheight="16">
+ <image source="../Sprites/tileType16.png" trans="ffffff" width="160" height="32"/>
+</tileset>
