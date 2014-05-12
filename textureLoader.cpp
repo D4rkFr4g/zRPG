5f56ecea75f22acf6559f5643560a950bd31a22a
tjuyeda <tjuyeda@gmail.com>
2014-05-12 13:28:59 -0700

    Loaded the enemy battle animations in Loader

--- textureLoader.cpp
+++ textureLoader.cpp
@@ -13,8 +13,11 @@ void textureLoader::loadTextures(std::unordered_map<std::string, Texture>* g_tex
 {
    int* width = new int;
 	int* height = new int;
-   
-   GLuint texture = glTexImageTGAFile("./Sprites/spriteSheet_chicken.tga", NULL, NULL);
+
+   GLuint texture = glTexImageTGAFile("./Sprites/link_Sprites.tga", NULL, NULL);
+   (*g_textures)["link"] = Texture(texture, *width, *height, 32, 32);
+
+   texture = glTexImageTGAFile("./Sprites/spriteSheet_chicken.tga", width, height);
    (*g_textures)["chicken"] = Texture(texture, 64, 64, 32, 32);
 
    texture = glTexImageTGAFile("./Sprites/pig_guard.tga", width, height);
@@ -29,8 +32,17 @@ void textureLoader::loadTextures(std::unordered_map<std::string, Texture>* g_tex
    texture = glTexImageTGAFile("./Sprites/link_battle.tga", width, height);
    (*g_textures)["link_battle"] = Texture(texture, *width, *height, 576, 64);
 
-   texture = glTexImageTGAFile("./Sprites/link_Sprites.tga", width, height);
-   (*g_textures)["link"] = Texture(texture, *width, *height, 32, 32);
+   texture = glTexImageTGAFile("./Sprites/chicken_battle_attack.tga", width, height);
+   (*g_textures)["chicken_attack"] = Texture(texture, *width, *height, 576, 64);
+
+   texture = glTexImageTGAFile("./Sprites/guard_battle_attack.tga", width, height);
+   (*g_textures)["guard_attack"] = Texture(texture, *width, *height, 576, 64);
+
+   texture = glTexImageTGAFile("./Sprites/knight_battle_attack.tga", width, height);
+   (*g_textures)["knight_attack"] = Texture(texture, *width, *height, 576, 64);
+
+   texture = glTexImageTGAFile("./Sprites/ganon_battle_jumpAttack.tga", width, height);
+   (*g_textures)["ganon_jumpAttack"] = Texture(texture, *width, *height, 576, 128);
 
    texture = glTexImageTGAFile("./Sprites/dialog16x16.tga", width, height);
    (*g_textures)["dialog"] = Texture(texture, *width, *height, 16, 16);
